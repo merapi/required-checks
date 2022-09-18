58ea2c60bfebeda143790a74184ff144ecca80ba
merapi <marcin@webskill.pl>
2022-09-18 16:15:17 +0200

    Fix tests

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reac
+          Learn React
         </a>
         <div>
           <button onClick={() => setCounter(c => c + 1)}>Increment {counter}</button>
